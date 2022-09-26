1e0b859efa5fcb20b129fe9220bae5faa00e1252
Pyr3z <levianperez@gmail.com>
2022-09-25 18:24:54 -0700

    fixed AndroidPlatformNotifier initializing too soon

--- Assets/Scripts/Schedule.cs
+++ Assets/Scripts/Schedule.cs
@@ -11,21 +11,26 @@ using TimeSpan = System.TimeSpan;
 [AssetPath("Resources/Schedule.asset")]
 public class Schedule : AssetSingleton<Schedule>
 {
+  private static IPlatformNotifier s_Notifier;
 
-#if UNITY_ANDROID
-  // only Android is planned for now... but you never know ~(￣▽￣)~*
-  private static readonly IPlatformNotifier s_Notifier = new AndroidPlatformNotifier();
-#endif
 
   [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
   private static void SubsystemInit()
   {
+    #if UNITY_ANDROID
+    // only Android is planned for now... but you never know ~(￣▽￣)~*
+    s_Notifier = new AndroidPlatformNotifier();
+    #endif
+
     s_Notifier.Initialize();
   }
 
 
   public static void TaskNotification([NotNull] TaskData task)
   {
+    if (KAssert.FailsNullCheck(s_Notifier))
+      return;
+
     // TODO handle resolution logic for user's chosen frequency + day condition parameters
 
     if (task.Frequency == FrequencyCategory.Once)
